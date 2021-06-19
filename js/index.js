ea5d6c9727aeae7b0bbb6b121d238c3868ec1f77
Jason Bellis <jcbellis@gmail.com>
2021-06-18 18:25:02 -0700

    initial commit CMF2015

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$('document').ready(function() {
+	var options = { videoId: 'ZvlT_2xi3YQ', start: 0 };
+	$('#wrapper').tubular(options);
+	// ZvlT_2xi3YQ  CMF clips  
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
